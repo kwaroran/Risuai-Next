3da1e0732fdff8723aaeb1892443229f3940d45d
kwaroran <amasio1234@proton.me>
2026-09-05 00:36:33 +0900

    feat: enhance database schema with messages, modules, and regexscripts

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,3 +1,4 @@
+import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const accounts = sqliteTable('accounts', {
@@ -40,5 +41,69 @@ export const chatSessions = sqliteTable('chatSessions', {
 	linkedModel: text('linkedModel').notNull(),
 
 	//linked prompt
-	linkedPrompt: text('linkedPrompt').notNull()
+	linkedPrompt: text('linkedPrompt').notNull(),
+
+	//last accessed
+	lastAccessedAt: text('created_at').notNull(),
+	
+	//creation time
+	createdAt: text('created_at').notNull().default(sql`(CURRENT_TIMESTAMP)`),
+
+	//toggles data
+	toggles: text('toggles', {mode: 'json'}).notNull()
 });
+
+export const messages = sqliteTable('messages', {
+	//id
+	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+
+	//session
+	chatSessionId: text('chatSessionId').notNull(),
+
+	//speaker, also known as senders module ID
+	speakerId: text('speakerId').notNull(),
+
+	//message content
+	message: text('message').notNull(),
+
+	//metadata
+	meta: text('meta', {mode: 'json'}).notNull(),
+
+	//owner user with read/write permission
+	//thou we can check the chatSessions, its here to reduce unnecessary sql calls
+	owner: text('owner').notNull(),
+
+	//creation time
+	createdAt: text('created_at').notNull().default(sql`(CURRENT_TIMESTAMP)`),
+
+	//position in the session, fractional indexing
+	position: text('position').notNull()
+})
+
+export const modules = sqliteTable('modules', {
+	//id
+	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+
+	//icon, image id is stored
+	icon: text('icon').notNull(),
+
+	//owner user
+	owner: text('owner').notNull(),
+})
+
+export const regexscripts = sqliteTable('regexscripts', {
+	//id
+	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+
+	//parent module's id
+	moduleId: text('moduleId').notNull(),
+
+	//expression of the regex, except the flag
+	regexExpression: text('regexExpression').notNull(),
+
+	//flag of the regex
+	regexFlag: text('regexFlag').notNull(),
+
+	//replacer
+	content: text('content').notNull()
+})
